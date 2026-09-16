eceb16f9182a8d7b743ce904f89e51f32942221d
Beatriz Cardozo <beatriz.a.cardozo28@gmail.com>
2026-09-15 22:59:43 -0300

    fix(backend): removing and adjusitng the models to the new functions

--- backend/src/main/java/com/wortgarden/backend/vocabulary/model/Flashcard.java
+++ backend/src/main/java/com/wortgarden/backend/vocabulary/model/Flashcard.java
@@ -6,31 +6,19 @@ import com.wortgarden.backend.vocabulary.enums.WordType;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-@Document("flashcards")
 public class Flashcard {
 
-    @Id
-    private String id;
-
-    private String userId;
     private WordType wordType;
     private String wordBase;
     private String translation;
     private WordDetails details;
     private List<Example> examples;
     private List<String> tags;
-    private Level level;
+    private CefrLevel cefrLevel;
     private Source source;
 
-    private int repetitions;
-    private int status;
-    private LocalDateTime lastReview;
-    private LocalDateTime nextReview;
 }
